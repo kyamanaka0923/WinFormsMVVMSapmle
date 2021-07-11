bc300efe29ef582aa85b9a880000e908800c88ba
Koji Yamanaka <kyamanaka0923@gmail.com>
2021-07-11 16:55:07 +0900

    [feature] 各種機能追加
    - 引数でオプション情報を取得するように変更

--- src/ChartSample.Forms/Program.cs
+++ src/ChartSample.Forms/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using CommandLine;
 using WinFormsMvvmSample.Views;
 
 namespace WinFormsMvvmSample
@@ -14,7 +15,40 @@ namespace WinFormsMvvmSample
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainView());
+
+            var options = GetOptions();
+
+            Application.Run(new MainView(options));
         }
+
+        /// <summary>
+        /// オプションを取得
+        /// </summary>
+        /// <returns></returns>
+        private static Options GetOptions()
+        {
+            var args = Environment.GetCommandLineArgs();
+
+            var parserResult = Parser.Default.ParseArguments<Options>(args);
+
+            if (parserResult.Tag != ParserResultType.Parsed) return null;
+            if (!(parserResult is Parsed<Options> parsed)) return null;
+            var opt = parsed.Value;
+
+            return opt;
+
+        }
+
     }
+
+    // コマンドラインオプション取得
+    public class Options
+    {
+        /// <summary>
+        /// Businessモデル名を入力
+        /// </summary>
+        [Option('B', "BU", Required = true, HelpText = "BUを入力する")]
+        public string Bu { get; set; }
+    }
+
 }
