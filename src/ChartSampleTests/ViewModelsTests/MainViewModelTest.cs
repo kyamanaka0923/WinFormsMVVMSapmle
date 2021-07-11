8f6dfe36192f1650d006a683194b8215f6a8600c
Koji Yamanaka <kyamanaka0923@gmail.com>
2021-07-11 17:23:20 +0900

    [test] ViewModelのUpdateメソッドテストを追加

--- src/ChartSampleTests/ViewModelsTests/MainViewModelTest.cs
+++ src/ChartSampleTests/ViewModelsTests/MainViewModelTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using System.Windows.Threading;
 using Castle.Core.Internal;
@@ -48,6 +49,31 @@ namespace ChartSampleTests.ViewModelsTests
             viewModel.Save();
 
             viewModel.AaaTextBoxText.Is("");
+
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            var messageServiceMock = new Mock<IMessageService>();
+
+            var machineDataServiceMock = new Mock<IMachineDataService>();
+
+            var viewModel = new MainViewModel(messageServiceMock.Object, machineDataServiceMock.Object, Dispatcher.CurrentDispatcher);
+            machineDataServiceMock.Setup(x => x.FindById(It.IsAny<string>())).Returns(
+                new List<MainViewModelGrid>()
+                {
+                    new MainViewModelGrid("1", "aaa", false),
+                    new MainViewModelGrid("2", "aaa", true),
+                    new MainViewModelGrid("3", "ccc", false)
+                }
+            );
+
+            viewModel.Update();
+
+            viewModel.AaaTextBoxText.Is("AAA updated!");
+            viewModel.MachineDataGridSource.Count.Is(3);
+
 
         }
         
